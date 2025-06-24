5363e1fc77ef13a85ea6c2557bee2eee74444c38
Iyare Usen <iyare.usen@gmail.com>
2025-06-24 05:18:11 +0100

    saves imported data to variable

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // "use strict";
-fetch("./data.json")
+const posts = fetch("./data.json")
 .then(response => {
     if (!response.ok) {
         throw new Error(`Data file could not be retrieved. Status code: ${response.status}`)
@@ -8,9 +8,9 @@ fetch("./data.json")
 }).then(data => {
     // data = JSON.parse(data);
     console.log(data)
-  
+    return data;
 }).catch(error => {
     console.error("There was a problem with the fetch operation", error)
 });
 
-// console.log(posts);
+console.log(posts)

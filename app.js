c9caeeb9014f6b57dcd5ac73e505874e362b8ad0
Iyare Usen <iyare.usen@gmail.com>
2025-06-24 04:48:51 +0100

    imports data file for posts

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-import { posts } from "/data.js";
+// "use strict";
+fetch("./data.json")
+.then(response => {
+    if (!response.ok) {
+        throw new Error(`Data file could not be retrieved. Status code: ${response.status}`)
+    }
+    return response.json();
+}).then(data => {
+    // data = JSON.parse(data);
+    console.log(data)
+  
+}).catch(error => {
+    console.error("There was a problem with the fetch operation", error)
+});
 
-console.log(posts)
+// console.log(posts);

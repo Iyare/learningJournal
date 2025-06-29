ff3af7d3ec2b2a2bd83256153b0a1501f7ec5be0
Iyare Usen <iyare.usen@gmail.com>
2025-06-30 00:03:40 +0100

    mobile view completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const articlesContainer = document.querySelector("#articles");
 const imageBaseUrl = "assets/post"
 
+
 fetch("./data.json")
     .then(response => {
         if (!response.ok) {
@@ -9,7 +10,37 @@ fetch("./data.json")
             return response.json();
         }
     })
-    .then(data => console.log(data))
+    .then(data => { 
+        data.forEach((post,index) => {
+            // create an article element
+            const article = document.createElement("article");
+             
+            // Create an image element for the post's image and set it's "alt" and "src" attributes
+            const postImage = document.createElement("img");
+            postImage.setAttribute("src", `${imageBaseUrl}-${index}.jpg`);
+            postImage.setAttribute("alt", `${post.title}`);
+            
+            // Create a span element and add the class of "date" to apply css stylings to it
+            const postDate = document.createElement("span");
+            // Assign element to object's title
+            postDate.innerText = post.date;
+            postDate.setAttribute("class", "date");
+            
+            // Create a heading element (h2) and assign it's content to the object's title
+            const postTitle= document.createElement("h2");
+            postTitle.textContent = post.title;
+            
+            // create a paragraph element and assign it's content to the post's content.
+            const postContent = document.createElement("p");
+            postContent.textContent = post.content;
+            
+            // Nest everything in the article element
+            article.append(postImage, postDate, postTitle, postContent)
+            
+            // render to UI by appending to articles DOM
+            articlesContainer.append(article)
+        })
+    })
     
 
 /* Workflow
@@ -25,26 +56,5 @@ fetch("./data.json")
 
  */
 
-/* posts.forEach((post, index) => {
-    
-    const article = document.createElement("article");
-    article.setAttribute("class", "container");
-    
-    const postImage = document.createElement("img");
-    postImage.setAttribute("src", `${imageBaseUrl}-${index}`);
-    postImage.setAttribute("alt", `${post.title}`);
-    
-    const postDate = document.createElement("span");
-    postDate.innerText = `${post.date}`;
-    
-    const postTitle= document.createElement("h2");
-    postTitle.textContent = `${post.title}`;
-    
-    const postContent = document.createElement("p");
-    postContent.textContent = `${post.content}`;
-    
-        
-    article.appendChild(postImage, postDate, postTitle, postContent)
-    console.log(article)
-})  */
+
 

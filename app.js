8432cb937b9e1139afc781a2c572c097cf664379
Iyare Usen <iyare.usen@gmail.com>
2025-06-29 20:31:36 +0100

    removes unnecessary codes for testing

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const articlesContainer = document.querySelector("#articles");
 const imageBaseUrl = "assets/post"
 
-
-const posts = fetch("./data.json")
-.then(response => {
-    if (!response.ok) {
-        throw new Error(`Data file could not be retrieved. Status code: ${response.status}`)
-    }
-    return response.json();
-}).then(data => {
-    // console.log(data)
+fetch("./data.json")
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Data file could not be retrieved. Status code: ${response.status}`)
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => console.log(data))
     
-    return data;
-}).catch(error => {
-    console.error("There was a problem with the fetch operation", error)
-});
-
-console.log(posts);
-
-function renderUI() {
-    articlesContainer.innerHTML = `<p>testing</p>`
-}
 
 /* Workflow
 1. Create an "article element"
